ad587c1eccce5e12c9fb0c33597789f6de46ea6b
Orlando G. Rodriguez <orlandogrodriguez94@gmail.com>
2017-11-25 00:01:55 -0400

    almost done...

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ $('#work-experience-detail').hide();
  * 2:   displays Delta Air Lines
  * 3:   displays ADP
  */
-var workExperienceSelected = 2;
+var workExperienceSelected = 0;
 
 var workExperience = {
     0: {"CompanyName": "Brain Trauma Assessment Protocols",
@@ -126,3 +126,25 @@ function clearAllBorders() {
     $('#delta').css("borderBottom", "0px solid #F23B46");
     $('#adp').css("borderBottom", "0px solid #F23B46");
 }
+
+var projects = {
+    3: {"ProjectName": "Brain Trauma Assessment Protocols",
+        "Position": "Product Developer",
+        "StartDate": "Aug 2017",
+        "EndDate": "May 2018",
+        "Description": " As the iOS development team leader, I've focused my efforts in designing the program architecture of the application itself. Using Swift and Xcode, I designed most of the application's elements and user interface, consisting of several view controllers, many of which I delegated to my teammates. Furthermore, we used GitHub to seamlessly merge all of our collaborations. Through team meetings and effective communication, we were able to create the first prototype of the application."},
+    0: {"ProjectName": "FreeBites",
+        "Position": "Lead iOS Developer",
+        "StartDate": "Jan 2017",
+        "EndDate": "May 2017",
+        "Description": "As an electrical engineering intern at Sto Corp, I was able to work on several important projects, and gained experience in the engineering of the construction materials manufacturing industry."},
+    1: {"ProjectName": "FridgeHound",
+        "Position": "Product Developer",
+        "StartDate": "March 2017",
+        "EndDate": "May 2017",
+        "Description": "As part of the Interiors Engineering department at Delta Air Lines Technical Operations, I've been able to aid in numerous projects as well as develop the necessary skills to take on projects independently in the near future.<br><br>My biggest contribution so far has been in the implementation of Gogo Inc's latest in-flight internet technology, 2KU, on Delta's B737-800 fleet. I've aided in design decisions, troubleshooting, and documentation."},
+    2: {"ProjectName": "Playground",
+        "Position": "Lead iOS Developer",
+        "StartDate": "Sep 2017",
+        "EndDate": "Present",
+        "Description": "As a Global Product Technology intern at ADP, I joined the Talent Management Solutions (TMS) team. During my time as part of this team, developed a Java web application that allows other TMS Software Developers to view and change log levels of TMS java web applications during runtime. This application handles log changes for local instances of TMS applications, but also the propagation of a log level changes throughout any and all servers running TMS applications simultaneously."}};
